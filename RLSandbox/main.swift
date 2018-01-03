20483e313602310cf6a042c7cd57b199347257b8
Steve Johnson <steve@steveasleep.com>
2018-01-02 21:37:31 -0800

    BearLibTerminal + Swift. It works!

--- RLSandbox/main.swift
+++ RLSandbox/main.swift
@@ -7,6 +7,104 @@
 //
 
 import Foundation
+import BearLibTerminal
+import GameplayKit
 
-print("Hello, World!")
+let DUNGEON: [String] = [
+  "###########################################################",
+  "#...........#.............................................#",
+  "#...........#........#....................................#",
+  "#.....................#...................................#",
+  "#....####..............#..................................#",
+  "#.......#.......................#####################.....#",
+  "#.......#...........................................#.....#",
+  "#.......#...........##..............................#.....#",
+  "#####........#......##.....@....##################..#.....#",
+  "#...#...........................#................#..#.....#",
+  "#...#............#..............#................#..#.....#",
+  "#...............................#..###############..#.....#",
+  "#...............................#...................#.....#",
+  "#...............................#...................#.....#",
+  "#...............................#####################.....#",
+  "#.........................................................#",
+  "#.........................................................#",
+  "###########################################################",
+]
 
+enum Terrain: Character {
+  case floor = "."
+  case wall = "#"
+
+  var cString: ContiguousArray<CChar> { return String([rawValue]).utf8CString }
+}
+extension int2: Hashable {
+  public var hashValue: Int {
+    return x.hashValue &* 31 &+ y.hashValue
+  }
+}
+
+struct Cell {
+  var terrain: Terrain
+}
+
+class TileMap {
+  var cells = [int2: Cell]()
+
+  func cell(_ point: int2) -> Cell? {
+    return cells[point]
+  }
+
+  func put(_ point: int2, _ cell: Cell) {
+    cells[point] = cell
+  }
+}
+
+func getAllowsLight(_ tilemap: TileMap, _ point: int2) -> Bool {
+  guard let cell = tilemap.cell(point) else { return false }
+  return cell.terrain == .floor
+}
+
+func draw(tilemap: TileMap, playerPos: int2) {
+  for (pt, cell) in tilemap.cells {
+    let chr = Array<Int8>(cell.terrain.cString)
+    terminal_print(pt.x, pt.y, UnsafePointer(chr))
+  }
+  let chr = Array<Int8>("@".utf8CString)
+  terminal_print(playerPos.x, playerPos.y, UnsafePointer(chr))
+}
+
+func main() {
+  terminal_open()
+
+  let tilemap = TileMap()
+  var playerPos = int2(0, 0)
+  for (y, row) in DUNGEON.enumerated() {
+    for (x, char) in row.enumerated() {
+      tilemap.put(int2(Int32(x), Int32(y)), Cell(terrain: Terrain(rawValue: char) ?? .floor))
+      if char == "@" {
+        playerPos = int2(Int32(x), Int32(y))
+      }
+    }
+  }
+
+  mainLoop: while true {
+    terminal_clear()
+    draw(tilemap: tilemap, playerPos: playerPos)
+    terminal_refresh()
+
+    if terminal_has_input() != 0 {
+      switch terminal_read() {
+      case TK_Q, TK_CLOSE: break mainLoop
+      case TK_UP: playerPos.y -= 1
+      case TK_DOWN: playerPos.y += 1
+      case TK_LEFT: playerPos.x -= 1
+      case TK_RIGHT: playerPos.x += 1
+      default: break
+      }
+    }
+  }
+
+  terminal_close()
+}
+
+main()

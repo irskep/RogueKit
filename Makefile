fd575ff8982ad2b7a09b75cad4cb236cfca4dbc5
Steve Johnson <steve@steveasleep.com>
2018-01-20 08:37:25 -0800

    Important bug fixes

--- Makefile
+++ Makefile
@@ -4,8 +4,15 @@ cp: Binaries/libBearLibTerminal.dylib
 	mkdir -p ./.build/debug
 	cp Binaries/libBearLibTerminal.dylib ./.build/debug/
 
+cp2: Binaries/libBearLibTerminal.dylib
+	mkdir -p ./.build/release
+	cp Binaries/libBearLibTerminal.dylib ./.build/release/
+
 build: cp
 	swift build -Xlinker -L./Binaries
+
+build-release: cp2
+	swift build -c release -Xlinker -L./Binaries
 
 run: build
 	./.build/debug/RogueKit\ Demo
--- Sources/RogueKit/main.swift
+++ Sources/RogueKit/main.swift
@@ -3,24 +3,15 @@ import BearLibTerminal
 
 print("Launched")
 
-//let RESOURES_PATH = "/Users/steve/_d/games/RogueKit/Resources"
-//
-//extension REXPaintImage {
-//  convenience init?(name: String) {
-//    guard let url = URL(string: "file://\(RESOURES_PATH)/xp/\(name).xp") else { return nil }
-//    guard let data = try? Data(contentsOf: url) else {
-//      print("Error loading", url)
-//      return nil
-//    }
-//    self.init(maybeGzippedData: data)
-//  }
-//}
-//
-//let sprites = SpriteSheet(image: REXPaintImage(name: "fabs")!).sprites
-//
-//runTest(rexPaintImage: REXPaintImage(name: "xptest")!, fontPath: "\(RESOURES_PATH)/cp437_10x10.png")
-
-let resources = ResourceCollection(path: CommandLine.arguments[1])
+var path = ""
+if CommandLine.arguments.count > 1 {
+  path = CommandLine.arguments[1]
+}
+if !FileManager.default.fileExists(atPath: path) {
+  path = Bundle.main.resourcePath! + "/Resources"
+}
+NSLog("\(path)")
+let resources = ResourceCollection(path: path)
 let terminal = BLTerminal.main
 terminal.open()
 

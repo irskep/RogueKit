788985db85a7af7a9eb02386fda9bce9a26546e7
Steve Johnson <steve@steveasleep.com>
2018-01-20 08:37:39 -0800

    Working standalone app

---
+++ RogueKitApp/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  RogueKitApp
+//
+//  Created by Steve Johnson on 1/19/18.
+//
+
+/*
+import RogueKit
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet weak var window: NSWindow!
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
+*/

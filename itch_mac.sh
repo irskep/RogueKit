db63fc75a227a41ba6e040dbe33076abd9f43383
Steve Johnson <steve@steveasleep.com>
2018-03-04 12:23:04 -0800

    Simplify itch upload script

--- itch_mac.sh
+++ itch_mac.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 cd .build/Products/Debug/ && \
-  zip -vr "../../Dr. Hallervorden.zip" "RogueKitApp.app" -x "*.DS_Store" && \
-  cd ../.. && \
+  zip -vr "Dr. Hallervorden.zip" "RogueKitApp.app" -x "*.DS_Store" && \
   butler push "Dr. Hallervorden.zip" irskep/dr-hallervorden:mac

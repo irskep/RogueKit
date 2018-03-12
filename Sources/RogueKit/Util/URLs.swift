458c0d72a1354b621333e7ea915c49acbcf91a3b
Steve Johnson <steve@steveasleep.com>
2018-03-12 12:47:54 -0700

    Linux fixes pt 3

--- Sources/RogueKit/Util/URLs.swift
+++ Sources/RogueKit/Util/URLs.swift
@@ -10,9 +10,21 @@ import Foundation
 
 class URLs {
   class var gameURL: URL? {
+    #if os(OSX)
     guard let appSupportURL = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask).first else {
       return nil
     }
+    #else
+      let appSupportURL = FileManager.default.homeDirectoryForCurrentUser.appendingPathComponent(".config").appendingPathComponent("dr-hallervorden")
+      if !FileManager.default.fileExists(atPath: appSupportURL.path) {
+        do {
+          try FileManager.default.createDirectory(atPath: appSupportURL.path, withIntermediateDirectories: true, attributes: nil)
+        } catch let e {
+          print(e)
+          fatalError("Can't create save directory")
+        }
+      }
+    #endif
     return appSupportURL.appendingPathComponent("game.json")
   }
 }
